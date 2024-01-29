4780cd78cb2ff47431d3560f4c9ca94099d0e5b8
jacob-stein1 <jmstein@bu.edu>
2024-01-29 00:29:06 -0500

    Test automated deployment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Azure Web App!');
+app.get("/", (req, res) => {
+  res.send("I hope this is automatically changed!");
 });
 
 app.listen(port, () => {
